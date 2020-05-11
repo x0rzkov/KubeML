5de3632413f92792eabcd45ceb3edd97532ad0bf
Harpreet <somel.harpreet@gmail.com>
2020-05-11 16:26:49 -0700

    changed redux persist mode from local storage to session storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,13 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import storageSession from "redux-persist/lib/storage/session";
 
 import userReducer from "./user/user.reducer";
 import plansAndPricingReducer from "./plans-and-pricing/plans-and-pricing.reducer";
 
 const persistConfig = {
   key: "root",
-  storage,
+  storage: storageSession,
   whitelist: ["plansAndPricing"],
 };
 
