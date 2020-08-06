d54ce722bd18064b157d118ed3a448f2908fab5f
Harpreet Somel <somel.harpreet@gmail.com>
2020-08-05 18:28:59 -0700

    modified readme

--- README.md
+++ README.md
@@ -5,24 +5,32 @@
 KubeML is a PaaS that directly competes with AWS SageMaker for building, training, and testing machine-learning models. KubeML provisions cloud infrastructure based on client needs and launches a Jupyter Enterprise deployment. Client's access JupyterHub for launching multiple kernels. KubeML deploys the identical, open-source Jupyter IDE environment as AWS SageMaker without the price mark-up.
 
 <!-- ![alt text](./assets/jhub-kernels.jpeg?raw=true) -->
+<br/>
+<h3>KubeML Jupyterhub Notebook</h3>
 <p align="center">
-  <img src="./assets/KubeML-kernels.jpeg" width="850" title="Kernels">
+  <img src="./assets/KubeML-kernels.jpeg" width="750" title="Kernels">
 </p>
+
 <br/>
+<h3>AWS SageMaker Jupyterhub Notebook</h3>
 <p align="center">
-  <img src="./assets/Kubeml-kernel.jpeg" width="850" title="Kernels">
+  <img src="./assets/SageMaker.png" width="750" title="Kernels">
 </p>
+
 <br/>
+<h3>KubeML Python3 Kernel</h3>
 <p align="center">
-  <img src="./assets/SageMaker.png" width="850" title="Kernels">
+  <img src="./assets/Kubeml-kernel.jpeg" width="750" title="Kernels">
 </p>
+
 <br/>
-<br/>
-<h3>SageMaker kernel</h3>
+<h3>SageMaker Python3 kernel</h3>
 <p align="center">
-  <img src="./assets/sagemaker-kernel.png" width="850" title="Kernels">
+  <img src="./assets/sagemaker-kernel.png" width="750" title="Kernels">
 </p>
+
 <br/>
+
 ## Advantages
 
 KubeML is significantly cheaper than AWS SageMaker (40% cheaper for on-demand usage, 65% cheaper for long-term usage).
---
+++ kubeml-frontend-server/client/build/precache-manifest.9517d71ff136d0bd165c35db5cab2fd4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9365942f92f44d3953e445974b2fd1f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0150466b4716dfef03d6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "587f703c469ec53532ee",
+    "url": "/static/css/main.cef73e29.chunk.css"
+  },
+  {
+    "revision": "0150466b4716dfef03d6",
+    "url": "/static/js/2.faffcc42.chunk.js"
+  },
+  {
+    "revision": "cca382c1052555c9153d6c53a44ba679",
+    "url": "/static/js/2.faffcc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587f703c469ec53532ee",
+    "url": "/static/js/main.f24cfed3.chunk.js"
+  },
+  {
+    "revision": "086656f679309a141372",
+    "url": "/static/js/runtime-main.ed6e40b8.js"
+  },
+  {
+    "revision": "9bfa935461ad997cad21f13e72e7e690",
+    "url": "/static/media/3d.9bfa9354.svg"
+  },
+  {
+    "revision": "505f2f91bda09b4e5e3cb7d1ec7da9e8",
+    "url": "/static/media/Jupyter-Notebook.505f2f91.svg"
+  },
+  {
+    "revision": "8217d2680f0dcb8bb921dce6cfa35ed8",
+    "url": "/static/media/JupyterHub.8217d268.svg"
+  },
+  {
+    "revision": "7276302428e58b6099edcf04154fd1a6",
+    "url": "/static/media/Scalability.72763024.svg"
+  },
+  {
+    "revision": "dcc6f07fda3d46bacfe9a9ac36f2bc2a",
+    "url": "/static/media/diagram-one.dcc6f07f.svg"
+  },
+  {
+    "revision": "14678449ba0f86748914b1ef1bc3a9f5",
+    "url": "/static/media/first-slide.14678449.svg"
+  },
+  {
+    "revision": "8b0d4e3e0e9d29bbe85b165fd1042f20",
+    "url": "/static/media/landing.8b0d4e3e.png"
+  },
+  {
+    "revision": "5c825e10fad10e0c4c4012ed3549ff67",
+    "url": "/static/media/second-slide.5c825e10.svg"
+  }
+]);
