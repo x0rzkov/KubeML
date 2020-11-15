13b585b4d0fdca92fd9511c6cf60a7476eaa5fbf
x0rzkov <x0rzkov@protonmail.com>
2020-11-15 13:30:11 +0100

    Added firebase env variables

--- kubeml-frontend-server/client/src/firebase/firebase.utils.js
+++ kubeml-frontend-server/client/src/firebase/firebase.utils.js
@@ -1,16 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+
+
+
 // ---------------------------------------------------------------------
 const config = {
-  apiKey: "AIzaSyBD87B8drIK2NNLr-5e4PMYMEDQmKDP8pw",
-  authDomain: "kube-ml.firebaseapp.com",
-  databaseURL: "https://kube-ml.firebaseio.com",
-  projectId: "kube-ml",
-  storageBucket: "kube-ml.appspot.com",
-  messagingSenderId: "550937035835",
-  appId: "1:550937035835:web:7f023a1d494c948592481e",
-  measurementId: "G-MVRXP6G97S",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 firebase.initializeApp(config);
 // ----------------------------------------------------------------------
