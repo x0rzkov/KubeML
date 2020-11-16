c8bd3378cfa71d534f058505b581d337f344bb45
x0rzkov <x0rzkov@protonmail.com>
2020-11-16 05:46:10 +0000

    Update firestore config

--- kubeml-frontend-server/client/src/firebase/firebase.utils.js
+++ kubeml-frontend-server/client/src/firebase/firebase.utils.js
@@ -2,18 +2,16 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-
-
 // ---------------------------------------------------------------------
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCJ65QobNW6hUlOJd4UhnDvxRePygPCPVM",
+  authDomain: "kukeml.firebaseapp.com",
+  databaseURL: "https://kukeml.firebaseio.com",
+  projectId: "kukeml",
+  storageBucket: "kukeml.appspot.com",
+  messagingSenderId: "664246499029",
+  appId: "1:664246499029:web:376ffde3301107e2b9145c",
+  measurementId: "G-CCE8N87XXK"
 };
 firebase.initializeApp(config);
 // ----------------------------------------------------------------------
